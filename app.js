63ff477bd9c4e7b059bd837196cf6a2a2fc25846
Mariusz Maciejczuk <mariusz.maciejczuk@intive.com>
2017-12-10 14:36:20 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/4.8.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCo3ZOn6BMPskoMm7ZiaSC3bMO0QyxgG4s",
+    authDomain: "web-quick-start-66485.firebaseapp.com",
+    databaseURL: "https://web-quick-start-66485.firebaseio.com",
+    projectId: "web-quick-start-66485",
+    storageBucket: "web-quick-start-66485.appspot.com",
+    messagingSenderId: "831562373541"
+  };
+  firebase.initializeApp(config);
+</script>
